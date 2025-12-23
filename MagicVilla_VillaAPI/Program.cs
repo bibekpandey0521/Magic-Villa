caa640fbba0098378afc9ee08f1bcfc76961872d
Bibek Pandey <bibek.pandey@dishhome.com.np>
2025-12-23 16:44:53 +0545

    LearnedAndExpolredContentNegoiatation

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -5,7 +5,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-
+//builder.Services.AddControllers().AddNewtonsoftJson();
+builder.Services.AddControllers(option =>
+{
+    option.ReturnHttpNotAcceptable = true;
+}).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
 //builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
